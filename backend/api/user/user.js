2a95abefe158c013e527d1bbae41a72b6171cb64
Jader Henrique de Paula <jader@compels.net>
2020-07-29 10:53:19 -0300

    implementing authentication in the backend API

--- backend/api/user/user.js
+++ backend/api/user/user.js
@@ -1,7 +1,7 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const userSchema = new mongoose.scheme({
+const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true, min: 6, max: 12 }
--- backend/config/routers.js
+++ backend/config/routers.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
-    const router = express.Router()
-    server.use('/api', router)
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const authService = require('../api/user/authService')
+    openApi.post('/login', authService.login)
+    openApi.post('/signup', authService.signup)
+    openApi.post('/validateToken', authService.validateToken)
+
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+
+    // const router = express.Router()
+    // server.use('/api', router)
 
     const billingCycleService = require('../api/billingCycle/billingCycleService')
-    billingCycleService.register(router, '/billingCycles')
+    billingCycleService.register(protectedApi, '/billingCycles')
 
     const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-    router.route('/billingSummary').get(billingSummaryService.getSummary)
+    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
 }
