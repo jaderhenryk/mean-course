8de505114b586c5032df0a10c38650c32ff93d1c
Jader Henrique de Paula <jader@compels.net>
2020-07-30 10:25:17 -0300

    Removing unused code

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BillingCycleComponent } from './billing-cycle/billing-cycle.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, PreloadAllModules } from '@angular/router';
-import { ListComponent } from './billing-cycle/list/list.component';
-import { FormComponent } from './billing-cycle/form/form.component';
-import { BillingCycleModule } from './billing-cycle/billing-cycle.module';
 
 export const ROUTES = [
     { path: '',  redirectTo: 'dashboard', pathMatch: 'full' },
